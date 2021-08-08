e864120eac470a8e138f593a1bd892b7b5b0cb76
potedev <quentin@pote.dev>
2021-08-08 19:40:34 +0200

    ORM - Added Prisma

---
+++ api/src/database/index.ts
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv'
+import path from 'path'
+import { PrismaClient } from '@prisma/client'
+
+//Nous avons besoin ici de remettre cette config avec dotenv 
+//Pour que Prisma puisse aller chercher où se trouve le fichier .env qui contiendra les informations de notre BDD
+const envPath = path.join(__dirname, '../../');
+dotenv.config({ path: envPath + '.env' })
+
+const prisma = new PrismaClient({ log: ['query'] })
+
+export { prisma }
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,8 +1,13 @@
 import { createServer } from './server';
 import { PORT } from './constants'
 
+import {prisma} from './database/index'
+
 const main = async () => {
     const server = await createServer();
+
+    const posts = await prisma.post.findMany();
+    console.log(posts);
 
     server.listen(PORT, () => {
         console.log(`Server is now running on port ${PORT}`)
