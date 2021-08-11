7bca8bf0933a2674415aa8a2a0607152b0f32477
potedev <quentin@pote.dev>
2021-08-11 16:39:35 +0200

    [API] Added an example route to fetch our posts with prisma

--- api/src/router.ts
+++ api/src/router.ts
@@ -1,10 +1,16 @@
 import { Router, Request, Response } from 'express'
+import {prisma} from './database/index'
 
 const mainRouter: Router = Router();
 
 //Voilà, notre première route !
 mainRouter.get('/', (_: Request, res: Response) => {
     res.json('Hello CPROM');
+})
+
+mainRouter.get('/posts', async (_: Request, res: Response) => {
+    const posts = await prisma.post.findMany();
+    res.send(posts);
 })
 
 export default mainRouter
